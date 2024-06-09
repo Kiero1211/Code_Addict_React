6fbec43323db352729fa1b90f55cbadc29f81c57
Kiero1211 <96629974+Kiero1211@users.noreply.github.com>
2024-06-09 16:18:51 +0700

    feat: load Tours component

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -1,33 +1,44 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, createContext } from 'react';
 import Loading from './Components/Loading/';
 import Tours from './Components/Tours/';
+import Tour from './Components/Tours/Tour';
 
 const url = "https://www.course-api.com/react-tours-project"
+const TourContext = createContext();
 
 function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
   const fetchTours = async () => {
-    const response = await fetch(url);
-    const tours = await response.json();
-    setTours(tours);
-  }
-
-  useEffect(() => {
     try {
-      fetchTours();
+      const response = await fetch(url);
+      const tours = await response.json();
+      setTours(tours);
     } catch (error) {
       console.log(error);
     } finally {
       setLoading(false)
-    }
-  }, [])
+    } 
+  }
+
+  useEffect(() => {
+    fetchTours();
+    }, [])
+    
+
+  const Component = loading ? <Loading/>
+                            : (
+                              <TourContext.Provider value={tours}>
+                                <Tours/>
+                              </TourContext.Provider>
+                              )
   return (
     <main>
-      {loading ? <Loading/> : <Tours/>}
+      {Component}
     </main>
   )
 }
+export {TourContext};
+export default App;
 
-export default App
--- 02-tours/setup/src/Components/Tours/index.js
+++ 02-tours/setup/src/Components/Tours/index.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, {useContext} from 'react';
+
+import { TourContext } from '../../App';
 import Tour from './Tour';
+
 const Tours = () => {
-  return <h2>tours component</h2>;
+  const tours = useContext(TourContext);
+    return (
+    <section>
+      <div className="title">
+        <h2>Our tours</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {tours.map(tour => <Tour key={tour.id} {...tour} />)}
+      </div>
+    </section>
+  );
 };
 
 export default Tours;
