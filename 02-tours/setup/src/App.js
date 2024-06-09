e3443fe23745b7ea65090ae1eb15166633bfbf88
Kiero1211 <96629974+Kiero1211@users.noreply.github.com>
2024-06-09 15:31:42 +0700

    set up Tours-Display

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -1,11 +1,33 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+import React, { useState, useEffect } from 'react';
+import Loading from './Components/Loading/';
+import Tours from './Components/Tours/';
+
+const url = "https://www.course-api.com/react-tours-project"
+
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    const response = await fetch(url);
+    const tours = await response.json();
+    setTours(tours);
+  }
+
+  useEffect(() => {
+    try {
+      fetchTours();
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false)
+    }
+  }, [])
+  return (
+    <main>
+      {loading ? <Loading/> : <Tours/>}
+    </main>
+  )
 }
 
 export default App
