97c770894cbbc0e73f5d231830fc87b1206ef8c6
Kiero1211 <96629974+Kiero1211@users.noreply.github.com>
2024-06-10 16:45:47 +0700

    feat: complete slider fucntions

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -1,11 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './constants';
 function App() {
   const [people, setPeople] = useState(data);
-  const [index, setIndex] = useState(0);
+  const [showIndex, setShowIndex] = useState(0);
+  const prevSlideRef = useRef();
+  const timerRef = useRef();
 
+  const returnValidIndex = useCallback((index) => {
+    if (index > data.length - 1) {
+      return 0;
+    } 
+    if (index < 0) {
+      return data.length - 1;
+    }
+    return index;
+  }, [])
+
+  const handleNext = () => setShowIndex(prev => returnValidIndex(prev + 1));
+
+  const handlePrevious = () => setShowIndex(prev => returnValidIndex(prev - 1));
+
+  useEffect(() => {
+    prevSlideRef.current = showIndex;
+  }, [showIndex])
+
+  useEffect(() => {
+    const timerID = setInterval(() => handleNext(), 3000)
+    timerRef.current = timerID;
+
+    return () => {
+      clearInterval(timerRef.current);
+    }
+  }, [showIndex])
   return (
     <section className="section"> 
     <div className="title">
@@ -17,9 +45,17 @@ function App() {
     <div className="section-center">
       {people.map((person, index) => {
         const {id, image, name, title, quote} = person;
+        let className;
+        if (index === showIndex) {
+          className = "activeSlide"
+        } else if (index === prevSlideRef.current) {
+          className = "lastSlide";
+        } else {
+          className = "nextSlide";
+        }
 
         return (
-          <article key={id}>
+          <article key={id} className={className}>
             <img src={image} alt={name} className="person-img" />
             <h4>{name}</h4>
             <p className="title">{title}</p>
@@ -28,10 +64,16 @@ function App() {
           </article>
         )
       })}
-      <button className="prev">
+      <button 
+        className="prev"
+        onClick={handlePrevious}
+      >
         <FiChevronLeft/>
       </button>
-      <button className="next">
+      <button 
+        className="next"
+        onClick={handleNext}
+      >
         <FiChevronRight/>
       </button>
     </div>
