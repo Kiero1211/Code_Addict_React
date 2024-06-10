1fe1b0a6b5b5c61bfe9efeee2a738db3ff64b07f
Kiero1211 <96629974+Kiero1211@users.noreply.github.com>
2024-06-10 15:55:19 +0700

    feat: setup tabs and slider

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import { FaAngleDoubleRight } from 'react-icons/fa'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
+import fetchData from "./constants";
 function App() {
+  const [tabs, setTabs] = useState([]);
+  useEffect(() => {
+    const requestedTabs = fetchData();
+    setTabs(requestedTabs);
+  }, [])
+  console.log(tabs);
   return <h2>tabs project setup</h2>
 }
 
