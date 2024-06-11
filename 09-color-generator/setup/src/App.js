20f4ded1006c13d9498537aa48008c6600920cc5
Kiero1211 <96629974+Kiero1211@users.noreply.github.com>
2024-06-11 14:35:48 +0700

    feat: set tup color picker

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -1,10 +1,37 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState } from 'react';
+import SingleColor from './SingleColor';
 
-import Values from 'values.js'
+import Values from 'values.js';
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let colors = new Values(color).all(10);
+  }
+
+  return (
+    <main>
+      <section className="container">
+        <h3>Color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input 
+            type="text" 
+            value={color} 
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#ffffff"
+          />
+          <button className="btn" type="submit">Submit</button>
+        </form>
+      </section>
+      <section className="colors">
+        <h4>List goes here</h4>
+      </section>
+    </main>
+  )
 }
 
 export default App
