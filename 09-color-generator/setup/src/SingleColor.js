6c85e56318930c769e0220159e7111033b1b5e14
Kiero1211 <96629974+Kiero1211@users.noreply.github.com>
2024-06-11 15:14:10 +0700

    feat: complete logic of color picker

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -1,17 +1,36 @@
-import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
+import React, { useState, useEffect, useRef } from 'react'
 
 const SingleColor = ({color, index, hexColor}) => {
+  const [alert, setAlert] = useState(false);
+
   const {rgb, weight} = color;
   const bcg = rgb.join(",");
   const hexValue = `#${hexColor}`
+
+  const handleClick = () => {
+    setAlert(true);
+    navigator.clipboard.writeText(hexValue);
+  }
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setAlert(false);
+    }, 2000);
+
+    return () => {
+      clearTimeout(timerId);
+    }
+  }, [alert])
+
   return (
     <article 
       style={{backgroundColor: `rgb(${bcg})`}} 
       className={`color ${index > 10 ? "color-light" : null}`}
+      onClick={handleClick}
     >
       <p className="percent-value">{weight}%</p>
       <p className="color-value">{hexValue}</p>
+      {alert && <p className="alert">Copied to clipboard</p>}
     </article>
   )
 }
