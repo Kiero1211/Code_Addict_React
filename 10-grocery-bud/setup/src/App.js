e40d627b3cf2d2889a4afd9b3f47adfd53296403
Kiero1211 <96629974+Kiero1211@users.noreply.github.com>
2024-06-11 16:29:40 +0700

    feat: complete logic of adding, editing & deleting grocery-items

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -3,7 +3,94 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEdit, setIsEdit] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({
+    show: false,
+    msg: "",
+    type: ""
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      // Error alert
+    } else if (name && isEdit) {
+      let found = false;
+      let foundIndex = -1;
+      for (let i = 0; i < list.length; i++) {
+        if (list[i].id === editID) {
+          found = true;
+          foundIndex = i;
+        }
+      }
+
+      if (!found) {
+        // Alert cannot find
+      }
+      // Edit list state
+      setList(prev => {
+        prev[foundIndex].title = name;
+        return prev;
+      })
+      setName("");
+      setIsEdit(false); 
+    } else {
+      const newJob = {
+        id: new Date().getTime().toString(),
+        title: name
+      }
+      setList(prev => [...prev, newJob]);
+      setName("");
+    }
+
+  }
+
+  const handleEdit = (item) => {
+    setEditID(item.id);
+    setIsEdit(true);
+    setName(item.title);
+  }
+
+  const handleDelete = (requestedID) => {
+    const newList = list.filter(item => item.id !== requestedID);
+    setList(newList);
+  }
+
+  return (
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>Grocery Bud</h3>
+        <div className="form-control">
+          <input 
+            type="text" 
+            className="grocery"
+            placeholder="e.g. Eggs"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+          <button type="submit" className="submit-btn">
+            {isEdit ? "Edit" : "Submit"}
+          </button>
+        </div>
+      </form>
+      { 
+        (list.length > 0) &&
+        <section className="grocery-container">
+          <List 
+            data={list}
+            handleEdit={handleEdit}
+            handleDelete={handleDelete}
+          />
+          <button className="clear-btn" onClick={() => setList([])}>Clear items</button>
+        </section>
+      }
+
+    </section>
+  )
 }
 
 export default App
--- 10-grocery-bud/setup/src/List.js
+++ 10-grocery-bud/setup/src/List.js
@@ -1,7 +1,27 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
+const List = ({ data, handleEdit, handleDelete}) => {
+
+  return (
+    <>
+      {data.map((item, index) => {
+        const {id, title} = item;
+        return (
+          <article key={id} className="grocery-item">
+            <p className="title">{title}</p>
+            <div className="btn-container">
+              <button className="edit-btn" onClick={() => handleEdit(item)}>
+                <FaEdit />
+              </button>
+              <button className="delete-btn" onClick={() => handleDelete(id)}>
+                <FaTrash />
+              </button>
+            </div>
+          </article>
+        )
+      })}
+    </>
+  )
 }
 
 export default List
