210df3527388f14b121e934f5c341e77239e3731
Kiero1211 <96629974+Kiero1211@users.noreply.github.com>
2024-06-14 17:49:08 +0700

    feat: complete sidebar & modal

--- 12-sidebar-modal/setup/src/Home.js
+++ 12-sidebar-modal/setup/src/Home.js
@@ -1,13 +1,15 @@
 import React, { useContext } from 'react'
 import { FaBars } from 'react-icons/fa'
+import { useGlobalContext } from './context'
 
 const Home = () => {
+  const {openSidebar, openModal} = useGlobalContext();
   return (
     <main>
-      <button className="sidebar-toggle">
+      <button className="sidebar-toggle" onClick={openSidebar}>
         <FaBars />
       </button>
-      <button className="btn">
+      <button className="btn" onClick={openModal}>
         Show modal
       </button>
     </main>
